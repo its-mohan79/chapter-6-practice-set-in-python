deff5cd6e866b00b986d95cd25206b3c2170dcfd
its-mohan79 <mohanroy8989@gmail.com>
2026-01-05 00:37:01 +0530

    solve the problem 5

---
+++ 05 problem5.py
@@ -0,0 +1,10 @@
+l=["mohan","jishan","ankit","vishal"]
+
+name=input("enter your name: ")
+
+if (name in l):
+    print("your name is in the list")
+
+else:
+    print("your name is not in the list")
+         
