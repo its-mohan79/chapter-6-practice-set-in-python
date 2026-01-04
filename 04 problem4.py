e870ed98a9548d6bc2cae44f48d4798bdb54c886
its-mohan79 <mohanroy8989@gmail.com>
2026-01-05 00:36:20 +0530

    solver the problem 4

---
+++ 04 problem4.py
@@ -0,0 +1,7 @@
+username=input("Enter username")
+
+if(len(username)<10):
+    print("your username is contains less than 10 character")
+
+else:
+    print("your username is contains equal to 10 character and more than")    
